ef0ad6c95a4dfba3ed34bf94f63ce099ea7c08ce
KhushiDhand <55299522+KhushiDhand@users.noreply.github.com>
2019-09-27 14:54:51 -0700

    gh

---
+++ StringAssignement/src/StringAssignment.java
@@ -0,0 +1,24 @@
+public class StringAssignment {
+        public static void main(String[] args) {
+            System.out.println("\t\t\t\t\t\t\t  |   BEAR   |");
+            System.out.println("MICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEMICKEYMOUSE@@@@@@@@@YMOUSEMICKEYMOU@@@@@@@@@YMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEMICKEYMOUS@         @MOUSEMICKEYM@             @YMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEMICKEYMOUS@           @OU@@@@@KE@              @KEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEMICKEYM@       @      @@@@@@@@@        @         @ICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEMICKE@       @   @                   @   @         @EYMOUSEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEMIC@         @   @                   @   @          @YMOUSEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEMI@            @                       @              @YMOUSEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEM@                                                    @OUSEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEM@                                                    @SEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEMO@                                                 @MOUSEMICKEYMOUSEMICKEYMOUSE");
+            System.out.println("MICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSEMICKEYMOUSE");
+
+
+        }
+}
+
+
+
+
+
