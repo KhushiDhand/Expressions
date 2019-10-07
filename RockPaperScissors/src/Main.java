6bf0b639d6048123733d29c42815feabce8cc4eb
KhushiDhand <55299522+KhushiDhand@users.noreply.github.com>
2019-10-07 14:00:58 -0700

    hu

--- RockPaperScissors/src/Main.java
+++ RockPaperScissors/src/Main.java
@@ -17,17 +17,18 @@ public class Main {
         rock = 1;
         paper = 2;
 
-        if (randomnumber == 0) {
+
+        if (randomnumber == 0) {              //if the random number is 0 then it will equal to s = scissors
             System.out.println("s");
         }
-        else if (randomnumber == 1){
+        else if (randomnumber == 1){           //if the random number is 1 then it will equal to p = paper
             System.out.println("p");
         }
         else{
-            System.out.println("r");
+            System.out.println("r");            //if the random number is 2 then it will equal to r = rock
         }
 
-        if(userChoice.equals("r")) {
+        if(userChoice.equals("r")) {            //if the random number is rock then these are the possiblities
             if (randomnumber == 0) {
                 System.out.println("The computer chose 's' so you win!");
             } else if (randomnumber == 1) {
@@ -36,28 +37,28 @@ public class Main {
                 System.out.println("The computer chose 'r' so it's a tie Try again!");
             }
         }
-            if (userChoice.equals("p")) {
-                if (randomnumber == 0) {
-                    System.out.println("The computer chose 's' so you lose! Try again!");
-                } else if (randomnumber == 1) {
-                    System.out.println("The computer chose 'p' so it's a tie! Try again!");
-                } else {
-                    System.out.println("The computer chose 'r' so you win!");
-                }
+        if (userChoice.equals("p")) {           //if the user choice is paper then these are the possiblities
+            if (randomnumber == 0) {
+                System.out.println("The computer chose 's' so you lose! Try again!");
+            } else if (randomnumber == 1) {
+                System.out.println("The computer chose 'p' so it's a tie! Try again!");
+            } else {
+                System.out.println("The computer chose 'r' so you win!");
             }
+        }
 
-                if (userChoice.equals("s")) {
-                    if (randomnumber == 0) {
-                        System.out.println("The computer chose 's' so it's a tie! Try again!");
-                    } else if (randomnumber == 1) {
-                        System.out.println("The computer chose 'p' so you win!");
-                    } else {
-                        System.out.println("The computer chose 'r' so you lose!");
-                    }
-                }
-
-                }
-
+        if (userChoice.equals("s")) {         //if the user choice is scissors then these are the possiblities
+            if (randomnumber == 0) {
+                System.out.println("The computer chose 's' so it's a tie! Try again!");
+            } else if (randomnumber == 1) {
+                System.out.println("The computer chose 'p' so you win!");
+            } else {
+                System.out.println("The computer chose 'r' so you lose!");
             }
+        }
+
+    }
+
+}
 
 
